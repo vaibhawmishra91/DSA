123253aa7e89e8b50a14df668e442d9c51783ba2
vaibhawmishra91 <vaibhawmishra86@gmail.com>
2026-08-19 18:17:51 +0530

    Time: 0 ms (100%), Space: 45.1 MB (29.47%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,15 +1,43 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        // int low=0;
-        // int high=nums.length-1;
-        // while(low<=high){
-        //     int mid=low+(high-low)/2;
-        //      if(nums[mid]==target) return mid;
+        int low = 0;
+        int high = nums.length - 1;
 
-        // }
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target) return true;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+                return true;
+
+            // Remove duplicates
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+            }
+
+            // Left half is sorted
+            else if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
         }
+
         return false;
     }
 }
+
+
+// for(int i=0;i<nums.length;i++){
+//             if(nums[i]==target) return true;
+//         }
+//         return false;
