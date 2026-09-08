2ce4a617211aec4b0e71e4dbfe47aa652c7a16c3
vaibhawmishra91 <vaibhawmishra86@gmail.com>
2026-09-08 22:55:28 +0530

    Time: 0 ms (100%), Space: 47.3 MB (48.89%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,23 +1,33 @@
-class Solution {
-    public int missingNumber(int[] nums) {
+// class Solution {
+//     public int missingNumber(int[] nums) {
         
-        Set<Integer> set=new HashSet<>();
-        for(int ele:nums){
-            set.add(ele);
-        }
+//         Set<Integer> set=new HashSet<>();
+//         for(int ele:nums){
+//             set.add(ele);
+//         }
 
-        int n=nums.length;
-        for(int i=0;i<=n;i++){
-            if(!set.contains(i)) return i;
-        }
-        return -1;
-    }
-}
+//         int n=nums.length;
+//         for(int i=0;i<=n;i++){
+//             if(!set.contains(i)) return i;
+//         }
+//         return -1;
+//     }
+// }
 // Time complexity--O(n)
 // Space Complexity--O(n);;
 
-// class Solution {
-//     public int missingNumber(int[] nums) {
-    
-//     }
-// }
+
+// Using bitwise operator
+class Solution {
+    public int missingNumber(int[] nums) {
+
+     int n = nums.length;
+        int xor = n;
+
+        for (int i = 0; i < n; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+
+        return xor;
+    }
+}
