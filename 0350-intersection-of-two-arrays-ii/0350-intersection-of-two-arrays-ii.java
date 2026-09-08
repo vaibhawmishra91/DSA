ef9388eebaa7ee62ee65e80dac296f690ddeddec
vaibhawmishra91 <vaibhawmishra86@gmail.com>
2026-09-08 22:05:37 +0530

    Time: 3 ms (94.77%), Space: 45 MB (66.1%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,28 +1,61 @@
+// class Solution {
+//     public int[] intersect(int[] nums1, int[] nums2) {
+//      Arrays.sort(nums1);   
+//      Arrays.sort(nums2);
+
+//      ArrayList<Integer> ans=new ArrayList<>();
+
+//      int i=0;
+//      int j=0;
+
+//      while(i<nums1.length && j<nums2.length){
+//         if(nums1[i]<nums2[j]) i++;
+//         else if(nums1[i]>nums2[j]) j++;
+//         else{
+//             ans.add(nums1[i]);
+//             i++;
+//             j++;
+//         }
+//      }
+//      int[] result = new int[ans.size()];
+
+//         for (int k = 0; k < ans.size(); k++) {
+//             result[k] = ans.get(k);
+//         }
+
+//         return result;
+//     }
+// }
+// Sorting + Two Pointers
+// time complexity--O(n log n + m log m)
+// space complexity---O(min(n,m))
+
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-     Arrays.sort(nums1);   
-     Arrays.sort(nums2);
 
-     ArrayList<Integer> ans=new ArrayList<>();
+       HashMap<Integer,Integer> map=new HashMap<>();
 
-     int i=0;
-     int j=0;
-
-     while(i<nums1.length && j<nums2.length){
-        if(nums1[i]<nums2[j]) i++;
-        else if(nums1[i]>nums2[j]) j++;
-        else{
-            ans.add(nums1[i]);
-            i++;
-            j++;
-        }
-     }
-     int[] result = new int[ans.size()];
-
-        for (int k = 0; k < ans.size(); k++) {
-            result[k] = ans.get(k);
+       // Count frequency of nums1
+       for (int ele : nums1) {
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
         }
 
-        return result;
+         ArrayList<Integer> ans=new ArrayList<>();
+          // Check nums2
+         for(int ele:nums2){
+            if(map.containsKey(ele) && map.get(ele) > 0){
+                ans.add(ele);
+            map.put(ele, map.get(ele) - 1);
+        }
+         } 
+
+         // Convert ArrayList<Integer> to int[]
+        int[] result = new int[ans.size()];
+
+        for (int i = 0; i < ans.size(); i++) {
+            result[i] = ans.get(i);
+        }
+
+        return result;    
     }
 }
