2dbd88a39359814b7aece16bae58161e88835c5a
vaibhawmishra91 <vaibhawmishra86@gmail.com>
2026-09-10 21:51:49 +0530

    Time: 25 ms (45.87%), Space: 48.8 MB (59.97%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,22 +1,47 @@
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+
+//         int count = 0;
+
+//         for (int i = 0; i < nums.length; i++) {
+
+//             int sum = 0;
+
+//             for (int j = i; j < nums.length; j++) {
+
+//                 sum += nums[j];
+
+//                 if (sum == k) {
+//                     count++;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+// }
+// Time ccomplexity-O(n²);
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
+      HashMap<Integer,Integer> map= new HashMap<>();
+    //   map conatin sum and map with its freq
+      
+       map.put(0,1);
 
-        int count = 0;
+      int count=0;
+      int sum=0;
+      
+      for(int i=0;i<nums.length;i++){
+      sum+=nums[i];
+      int rem=sum-k;
+      
+    //   value stored for rem; if rem does not exist, give me 0
+      count+=map.getOrDefault(rem,0);
 
-        for (int i = 0; i < nums.length; i++) {
-
-            int sum = 0;
-
-            for (int j = i; j < nums.length; j++) {
-
-                sum += nums[j];
-
-                if (sum == k) {
-                    count++;
-                }
-            }
-        }
-
-        return count;
+      // Store frequency of current prefix sum
+      map.put(sum,map.getOrDefault(sum,0)+1);
+      }
+      return count;
     }
 }
