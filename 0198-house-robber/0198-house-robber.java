765fb13e0a7cb9138a153008cfddbd73613bb9c3
vaibhawmishra91 <vaibhawmishra86@gmail.com>
2026-09-17 21:36:42 +0530

    Time: 0 ms (100%), Space: 42.8 MB (56.66%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -50,6 +50,35 @@
 //     }
 // }
 
+// class Solution {
+//     public int rob(int[] nums) {
+
+//         int n = nums.length;
+
+//         if (n == 1)
+//             return nums[0];
+
+//         int[] dp = new int[n];
+
+//         dp[0] = nums[0];
+//         dp[1] = Math.max(nums[0], nums[1]);
+
+//         for (int i = 2; i < n; i++) {
+
+//             int pick = nums[i] + dp[i - 2];
+
+//             int noPick = dp[i - 1];
+
+//             dp[i] = Math.max(pick, noPick);
+//         }
+
+//         return dp[n - 1];
+//     }
+// }
+// Time:  O(n)
+// Space: O(n)
+
+
 class Solution {
     public int rob(int[] nums) {
 
@@ -58,22 +87,24 @@ class Solution {
         if (n == 1)
             return nums[0];
 
-        int[] dp = new int[n];
+        
+        int prev2=0;
+        int prev1 = nums[0];
+        int ans=0;
+        
 
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        for (int state = 2; state <n+1; state++) {
 
-        for (int i = 2; i < n; i++) {
+            int pick = nums[state-1] +prev2 ;
 
-            int pick = nums[i] + dp[i - 2];
+            int noPick = 0+prev1;
 
-            int noPick = dp[i - 1];
+            ans = Math.max(pick, noPick);
 
-            dp[i] = Math.max(pick, noPick);
+            prev2=prev1;
+            prev1=ans;
         }
 
-        return dp[n - 1];
+        return ans;
     }
 }
-// Time:  O(n)
-// Space: O(n)
