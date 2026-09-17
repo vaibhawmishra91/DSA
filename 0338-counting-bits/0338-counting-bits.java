075b62d7a8ee8734d87c8b3f72599471b6038d45
vaibhawmishra91 <vaibhawmishra86@gmail.com>
2026-09-17 12:18:55 +0530

    Time: 2 ms (95.71%), Space: 48.5 MB (69.72%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,16 +1,31 @@
+// class Solution {
+//     public int[] countBits(int n) {
+        
+//         int[] ans=new int[n+1];
+//         for(int i=0;i<=n;i++){
+//             int num=i;
+//             int count=0;
+//             while(num>0){
+//                 num=num & (num-1);
+//                 count++;
+//             }
+//             ans[i]=count;
+//         }
+//         return ans;
+//     }
+// }
+// Time Complexity: O(n log n)
+// Space Complexity: O(n)
+
 class Solution {
     public int[] countBits(int n) {
-        
-        int[] ans=new int[n+1];
-        for(int i=0;i<=n;i++){
-            int num=i;
-            int count=0;
-            while(num>0){
-                num=num & (num-1);
-                count++;
-            }
-            ans[i]=count;
-        }
-        return ans;
+  int[] dp=new int[n+1];
+
+  for(int i=0;i<=n;i++){
+    dp[i]=dp[i>>1] + (i&1);
+  }
+  return dp;
     }
 }
+// Time: O(n) ✅
+// Space: O(n) ✅
